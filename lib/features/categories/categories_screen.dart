58024a49495a50d99b744ee82dc2e03febe5550c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-08-06 10:55:53 -0300

    starting app

---
+++ lib/features/categories/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text('Teste'),
+          Text('Teste'),
+          Text('Teste'),
+          Text('Teste'),
+          Text('Teste'),
+          Text('Teste'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/features/categories/categories_screen.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(217, 255, 73, 73),
+  ),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
