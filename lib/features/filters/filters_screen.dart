cdc26375d80c11b3dabd4e17d81334f0dd7e993f
JoaoVitorLima242 <jlima0500@gmail.com>
2023-08-11 23:53:28 -0300

    using willPopScope

--- lib/features/filters/filters_screen.dart
+++ lib/features/filters/filters_screen.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/features/filters/widgets/filters_check_item.dart';
 import 'package:meals_app/features/tab_bar/widgets/main_drawer.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -12,10 +19,10 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreen extends State<FiltersScreen> {
-  bool _gluttenFreeFilterSet = false;
-  bool _lactoseFreeFilterSet = false;
-  bool _vegetarianFilterSet = false;
-  bool _veganFilterSet = false;
+  bool _gluttenFreeFilter = false;
+  bool _lactoseFreeFilter = false;
+  bool _vegetarianFilter = false;
+  bool _veganFilter = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,48 +31,60 @@ class _FiltersScreen extends State<FiltersScreen> {
         title: const Text('Your Filters'),
       ),
       drawer: const MainDrawer(currentPage: DrawerRoutes.filters),
-      body: Column(children: [
-        FilterCheckItem(
-          title: "Gluten-free",
-          subtitle: "Only include gluten-free meals",
-          value: _gluttenFreeFilterSet,
-          onChanged: (isChecked) {
-            setState(() {
-              _gluttenFreeFilterSet = isChecked;
-            });
-          },
-        ),
-        FilterCheckItem(
-          title: "Lactose-free",
-          subtitle: "Only include lactose-free meals",
-          value: _lactoseFreeFilterSet,
-          onChanged: (isChecked) {
-            setState(() {
-              _lactoseFreeFilterSet = isChecked;
-            });
-          },
-        ),
-        FilterCheckItem(
-          title: "Vegetarian",
-          subtitle: "Only include vegetarian meals",
-          value: _vegetarianFilterSet,
-          onChanged: (isChecked) {
-            setState(() {
-              _vegetarianFilterSet = isChecked;
-            });
-          },
-        ),
-        FilterCheckItem(
-          title: "Vegan",
-          subtitle: "Only include vegan meals",
-          value: _veganFilterSet,
-          onChanged: (isChecked) {
-            setState(() {
-              _veganFilterSet = isChecked;
-            });
-          },
-        ),
-      ]),
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop({
+            Filter.glutenFree: _gluttenFreeFilter,
+            Filter.lactoseFree: _lactoseFreeFilter,
+            Filter.vegetarian: _vegetarianFilter,
+            Filter.vegan: _veganFilter,
+          });
+
+          return false;
+        },
+        child: Column(children: [
+          FilterCheckItem(
+            title: "Gluten-free",
+            subtitle: "Only include gluten-free meals",
+            value: _gluttenFreeFilter,
+            onChanged: (isChecked) {
+              setState(() {
+                _gluttenFreeFilter = isChecked;
+              });
+            },
+          ),
+          FilterCheckItem(
+            title: "Lactose-free",
+            subtitle: "Only include lactose-free meals",
+            value: _lactoseFreeFilter,
+            onChanged: (isChecked) {
+              setState(() {
+                _lactoseFreeFilter = isChecked;
+              });
+            },
+          ),
+          FilterCheckItem(
+            title: "Vegetarian",
+            subtitle: "Only include vegetarian meals",
+            value: _vegetarianFilter,
+            onChanged: (isChecked) {
+              setState(() {
+                _vegetarianFilter = isChecked;
+              });
+            },
+          ),
+          FilterCheckItem(
+            title: "Vegan",
+            subtitle: "Only include vegan meals",
+            value: _veganFilter,
+            onChanged: (isChecked) {
+              setState(() {
+                _veganFilter = isChecked;
+              });
+            },
+          ),
+        ]),
+      ),
     );
   }
 }
